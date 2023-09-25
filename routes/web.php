37d95f2c091a15c9d974a418601562059f84fae9
ricksonl.berigo <ricksonl.berigo@outlook.com>
2023-09-25 11:34:31 -0300

    Agrupando actions nas nossas rotas

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/series');
 });
 
-// My First Route
-Route::get('/series', [SeriesController::class, 'index']);
-
-Route::get('/series/create', [SeriesController::class, 'create']);
-
-Route::post('/series/save', [SeriesController::class, 'store']);
+Route::controller(SeriesController::class)->group(function () {
+    Route::get('/series', 'index');
+    Route::get('/series/create', 'create');
+    Route::post('/series/save', 'store');
+});
